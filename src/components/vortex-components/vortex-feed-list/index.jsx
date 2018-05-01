1cb3b58ebe4182c77ace35f7499a59710793d3e3
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-05-01 10:41:36 +0200

    WiP: Managed to render only on filter change
    VTXC #5

--- src/components/vortex-components/vortex-feed-list/index.jsx
+++ src/components/vortex-components/vortex-feed-list/index.jsx
@@ -4,10 +4,19 @@ import { connect } from '../../../utils/connect';
 class _VortexFeedList extends React.Component {
     constructor(props) {
         super(props);
+        this.feedsize = 0;
         const web3 = Vortex.get().Store.getState().web3;
-        web3._.eth.sendTransaction({ from: web3.coinbase, value: 2345, to: "0x198210f1E859f51cCf65f484CDC4B4669DAba3F3" });
+        web3._.eth.vortexSendTransaction({ from: web3.coinbase, value: 2345, to: "0x198210f1E859f51cCf65f484CDC4B4669DAba3F3" });
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.feed.length != this.feedsize) {
+            this.feedsize = nextProps.feed.length;
+            return true;
+        }
+        return false;
     }
     render() {
+        console.log(this.props.feed);
         return <p>ok</p>;
     }
 }
--- src/components/vortex-components/vortex-feed-list/index.tsx
+++ src/components/vortex-components/vortex-feed-list/index.tsx
@@ -13,13 +13,25 @@ interface VortexFeedListInternals {
 }
 
 class _VortexFeedList extends React.Component<VortexFeedListProps> {
+
+    private feedsize: number = 0;
+
     constructor(props: VortexFeedListProps) {
         super(props);
         const web3: any = Vortex.get().Store.getState().web3;
-        web3._.eth.sendTransaction({from: web3.coinbase, value: 2345, to: "0x198210f1E859f51cCf65f484CDC4B4669DAba3F3"});
+        web3._.eth.vortexSendTransaction({from: web3.coinbase, value: 2345, to: "0x198210f1E859f51cCf65f484CDC4B4669DAba3F3"});
+    }
+
+    public shouldComponentUpdate(nextProps: VortexFeedListProps, nextState: any): boolean {
+        if (nextProps.feed.length != this.feedsize) {
+            this.feedsize = nextProps.feed.length;
+            return true;
+        }
+        return false;
     }
 
     public render(): React.ReactNode {
+        console.log(this.props.feed);
         return <p>ok</p>;
     }
 }
