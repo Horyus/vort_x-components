1a3ef0fc1ba9040e6b183822b9dd4fb2d4a6c892
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-05-04 18:30:02 +0200

    Add exports
    VTXC #12

--- src/index.jsx
+++ src/index.jsx
@@ -4,3 +4,4 @@ export * from './components/vortex-components/vortex-gate';
 export * from './components/vortex-components/vortex-feed-list';
 export * from './utils/connect';
 export * from './utils/VortexMetamaskLoader';
+export { VortexContract } from 'vort_x';
