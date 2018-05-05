ebf8ad7731c284f3423e4b4fcfbbd5a146e906e3
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-05-05 15:48:17 +0200

    Add optional props for extended support
    VTXC #4

--- src/components/vortex-components/vortex-gate/index.tsx
+++ src/components/vortex-components/vortex-gate/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import {Vortex, State} from 'vort_x';
 import {Gatelock} from "./gatelock";
+import {DeepPartial, ReducersMapObject} from "redux";
 
 export interface VortexGateProps<T extends State> {
     loader: Promise<any>,
     contracts: any[],
-    network_contracts?: any[]
+    network_contracts?: any[],
+    reducers_map?: ReducersMapObject<T>,
+    custom_state?: DeepPartial<T>
 }
 
 export class VortexGate<T extends State = State> extends React.Component<VortexGateProps<T>> {
@@ -15,7 +18,7 @@ export class VortexGate<T extends State = State> extends React.Component<VortexG
     constructor(props: VortexGateProps<T>) {
         super(props);
 
-        this.vortex = Vortex.factory<T>(this.props.contracts, this.props.loader);
+        this.vortex = Vortex.factory<T>(this.props.contracts, this.props.loader, this.props.reducers_map, this.props.custom_state);
         if (this.props.network_contracts) {
             this.props.network_contracts.forEach((contract: any): void => {
                 this.vortex.networksOf(contract);
@@ -35,4 +38,4 @@ export class VortexGate<T extends State = State> extends React.Component<VortexG
 
 }
 
-export * from './organizers';
+export {VortexWeb3Loaded, VortexWeb3Loading, VortexWeb3LoadError, VortexWeb3NetworkError} from './organizers';
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-export * from './components/vortex-components/vortex-transactions-list';
-export * from './components/vortex-components/vortex-method-call-list';
-export * from './components/vortex-components/vortex-contracts-list';
-export * from './components/vortex-components/vortex-gate';
-export * from './components/vortex-components/vortex-feed-list';
-export * from './utils/connect';
-export * from './utils/VortexMetamaskLoader';
+export {VortexTransactionsListProps, VortexTransactionsList} from './components/vortex-components/vortex-transactions-list';
+export {VortexMethodCallListProps, VortexMethodCallList} from './components/vortex-components/vortex-method-call-list';
+export {VortexContractsListProps, VortexContractsList} from './components/vortex-components/vortex-contracts-list';
+export {VortexGateProps, VortexGate, VortexWeb3Loaded, VortexWeb3Loading, VortexWeb3LoadError, VortexWeb3NetworkError} from './components/vortex-components/vortex-gate';
+export {VortexFeedListProps, VortexFeedList, FeedType} from './components/vortex-components/vortex-feed-list';
+export {connect} from './utils/connect';
+export {VortexMetamaskLoader} from './utils/VortexMetamaskLoader';
 export {VortexContract} from 'vort_x';
